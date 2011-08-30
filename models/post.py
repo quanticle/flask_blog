1c2bc480fe01c587788eb481e5f8b0e1e6e95423
Rohit Patnaik (quanticle) <quanticle@gmail.com>
2011-08-29 22:10:26 -0500

    Added model classes for user and post

---
+++ models/post.py
@@ -0,0 +1,29 @@
+import sqlite3
+import user
+
+def retrieve_all_posts(db_path):
+    """Returns a list of all posts from the blog"""
+    pass
+
+def retrieve_post(db_path, post_id):
+    """Retrieves a specific post from the blog"""
+    pass
+
+
+class Post:
+    def get_title():
+        pass
+    def get_text():
+        pass
+    def get_date():
+        pass
+    def get_id():
+        pass
+    def set_title():
+        pass
+    def set_date():
+        pass
+    def set_text():
+        pass
+    def save(): 
+        pass
