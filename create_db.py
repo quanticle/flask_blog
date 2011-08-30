b1f258ab73898ae71ad74e6b0652d59ee48159bf
Rohit Patnaik (quanticle) <quanticle@gmail.com>
2011-08-29 21:29:05 -0500

    Fixed up foreign key constraints SQL errors

--- create_db.py
+++ create_db.py
@@ -1,7 +1,9 @@
 import sqlite3
 
 create_user_sql = """CREATE TABLE users (username TEXT, password_hash TEXT, salt TEXT)"""
-create_post_sql = """CREATE TABLE posts (post_title TEXT, post_date TEXT, post_file TEXT, FOREIGN KEY(post_author) REFERENCES users(username))"""
+create_post_sql = """CREATE TABLE posts (post_title TEXT, post_date TEXT, post_file TEXT, post_author TEXT,
+FOREIGN KEY(post_author) REFERENCES users(username))"""
+
 def create_database():
     db_conn = sqlite3.connect("blog_db.sqlite3")
     db_curs = db_conn.cursor()
