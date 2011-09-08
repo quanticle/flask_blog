d249e9cc5295ec3427960fc0cd28cf28dbb162d9
Rohit Patnaik <RPatnaik@intradyn.com>
2011-09-08 16:05:09 -0500

    Just messing around with some stuff

--- models/user.py
+++ models/user.py
@@ -1,4 +1,26 @@
 import sqlite3
 
 class User:
-    pass
+    def __init__(self, username, password_hash, real_name):
+        self.username = username
+        self.password_hash = password_hash
+        self.real_name = real_name
+
+    def get_username(self):
+        return self.username
+
+    def get_password_hash(self):
+        return self.password_hash
+
+    def get_real_name(self):
+        return self.real_name
+
+    def set_username(self, username):
+        self.username = username
+
+    def set_password_hash(self, new_password_hash):
+        self.password_hash = new_password_hash
+
+    def set_real_name(self, new_real_name):
+        self.real_name = new_real_name
+
