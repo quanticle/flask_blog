1d682a2eb8bc140e203f6208d710883989eb2a6d
Rohit Patnaik (quanticle) <quanticle@gmail.com>
2011-09-14 00:55:37 -0500

    Worked on user model and controller

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -2,5 +2,19 @@
 
 import sqlite3
 
-class UserController
+class UserController:
+    def __init__(self, db_connection):
+        self.db_connection = db_connection
     
+    def get_user(self, username):
+        select_user_sql = "SELECT username, password_hash, salt FROM users WHERE username = ?"
+        cursor = self.db_connection.cursor()
+        cursor.execute(select_user_sql, username)
+        row_list = cursor.fetchall()
+        if len(row_list) < 1:
+            return None
+        else:
+            db_row = row_list[0]
+            retrieved_user = User(db_row['username'], db_row['password_hash'], db_row['salt'])
+            return retrieved_user
+
--- models/user.py
+++ models/user.py
@@ -1,4 +1,16 @@
 import sqlite3
 
 class User:
-    pass
+    def __init__(self, username, password_hash, salt):
+        self.username = username
+        self.password_hash = password_hash
+        self.salt = salt
+    
+    def get_username(self):
+        return self.username
+
+    def get_password_hash(self):
+        return self.password_hash
+
+    def get_salt(self):
+        return self.salt
