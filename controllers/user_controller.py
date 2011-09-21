118e6f7fa8ff61a84fc52cb5d77f80b7d122210f
Rohit Patnaik (quanticle) <quanticle@gmail.com>
2011-09-20 22:43:44 -0500

    Got add and retrieve working on user controller

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -12,19 +12,21 @@ class UserController:
         db_cursor = self.db_connection.cursor() 
         db_cursor.execute(sql, (user.get_username(), user.get_real_name(), user.get_password_hash(), user.get_salt()))
         db_cursor.close()
+        self.db_connection.commit()
 
     def get_user(self, username):
         """Retrieves a user from the database, given a username"""
-        select_user_sql = "SELECT username, password_hash, realname, salt FROM users WHERE username = ?"
+        select_user_sql = r"SELECT username, password_hash, realname, salt FROM users WHERE username = ?"
         cursor = self.db_connection.cursor()
-        cursor.execute(select_user_sql, username)
+        cursor.execute(select_user_sql, [username])
         row_list = cursor.fetchall()
         if len(row_list) < 1:
             cursor.close()
             return None
         else:
             db_row = row_list[0]
-            retrieved_user = User(db_row['username'], db_row['password_hash'], db_row['realname'], db_row['salt'])
+            #SQLite 3, Y U NO USE DICTIONARY TO RETURN DATA?
+            retrieved_user = User(db_row[0], db_row[1], db_row[2], db_row[3])
             cursor.close()
             return retrieved_user
 
