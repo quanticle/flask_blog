979ab98cf672cd169833668d4ae488b93d8d79f1
Rohit Patnaik (quanticle) <quanticle@gmail.com>
2011-09-21 23:04:45 -0500

    Messed about with post_controller

--- controllers/post_controller.py
+++ controllers/post_controller.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 
 class PostController:
-    def __init__(self, db_location, post_folder):
+    def __init__(self, db_connection, post_folder):
         self.db_location = db_location
         self.post_folder = post_folder
 
