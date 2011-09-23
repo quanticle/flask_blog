26e548becec1c59e0ab61392f6f2e35e16eae7e6
Rohit Patnaik (quanticle) <quanticle@gmail.com>
2011-09-22 21:50:34 -0500

    Started work on post_controller

--- controllers/post_controller.py
+++ controllers/post_controller.py
@@ -5,5 +5,9 @@ class PostController:
     def __init__(self, db_connection, post_folder):
         self.db_location = db_location
         self.post_folder = post_folder
+    
+    def get_post(self, post_id):
+        db_cursor = self.db_connection.cursor()
+        
 
     
