ddcfaa9481dea9c26187fdda6bfe7d0908357f8f
Rohit Patnaik <RPatnaik@intradyn.com>
2011-10-13 09:36:54 -0500

    Got started on add post form

--- blog.py
+++ blog.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from views import all_posts
+from views import all_posts, add_post_form
 from config.blog_config import config
 import sqlite3
 
@@ -11,6 +11,14 @@ def root():
     db_connection = sqlite3.connect(db_path)
     return all_posts.show_all_posts(db_connection)
 
+@app.route('/add_post/')
+def add_post():
+    if request.method == "POST":
+        post_title = request.form['post_title']
+        post_text = request.form['post_text']
+        add_post_form.add_post(title, text)
+    else: 
+        return add_post_form.show_add_post()
 
 if __name__ == '__main__':
     app.run()
---
+++ views/add_post_form.py
@@ -0,0 +1,14 @@
+from jinja2 import Environment, PackageLoader
+from models.post import Post
+from models.user import User
+from controllers.post_controller import PostController
+from controllers.user_controller import UserController
+from config.blog_config import config
+
+def add_post(post_title, post_text):
+    post_author = 
+def show_add_post():
+    template_environment = Environment(loader=PackageLoader('views', 'templates'))
+    template = template_environment.get_template('add_post.html')
+    return template.render(blog_title=config['blog_title'])
+    
